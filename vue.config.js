73465c66afc6a2c475cc3c8c102d904caef1095d
jgduhao <18324706909@163.com>
2021-06-20 16:11:41 +0800

    animerss配置修改

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,13 @@ module.exports = {
                 changeOrigin: true,
             }
         }
+    },
+    publicPath: '/',
+    chainWebpack: config => {
+        //设置标题  默认不设置的话是项目名字
+        config.plugin('html').tap(args => {
+          args[0].title = "AnimeRSS"
+          return args
+        })
     }
 }
